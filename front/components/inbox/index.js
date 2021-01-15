af5a852f73d9efac8840032310ea4b058c10ac1a
Taylor Thompson <jamestthompson3@gmail.com>
2021-01-15 12:01:39 +0200

    Adds ability to expand and collapse inboxes

--- front/components/inbox/index.js
+++ front/components/inbox/index.js
@@ -1,6 +1,9 @@
 import Card from "../Card.js";
 import Component from "../Component.js";
 import Cancel from "../icons/Cancel.js";
+import Collapse from "../icons/Collapse.js";
+import Expand from "../icons/Expand.js";
+import Add from "../icons/Add.js";
 import { debounceEvent } from "../../utils/rendering.js";
 import { postData, messages, appContext } from "../../messages.js";
 
@@ -16,21 +19,18 @@ class Inbox extends Component {
     parent.innerHTML = `
       <div class="inbox actions">
         <h2 class="inbox title">${inbox.name}</h2>
-        <button title="add card to inbox" class="inbox add-card" aria-label="add card to inbox">➕</button>
+            <div id="inbox-action-indicator">
+              <button title="collapse view" class="inbox collapse" aria-label="collapse view">${Expand()}</button>
+              <button title="add card to inbox" class="inbox add-card" aria-label="add card to inbox">${Add()}</button>
+            </div>
+          </div>
       </div>
     `;
     // add event listeners
     const addButton = parent.querySelector(".inbox.add-card");
     addButton.addEventListener("click", this.openForm);
-    // create cards
-    const cards = inbox.cards;
-    cards.forEach(card => {
-      const cardContainer = document.createElement("div");
-      cardContainer.classList.add("card", "container");
-      cardContainer.dataset.key = card.id;
-      parent.appendChild(cardContainer);
-      new Card(cardContainer, { card });
-    });
+    const collapseButton = parent.querySelector(".inbox.collapse");
+    collapseButton.addEventListener("click", () => this.toggleCollapse());
     parent.addEventListener("click", this.clickAway, false);
     const boxTitle = parent.querySelector(".inbox.title");
     if (boxTitle) {
@@ -58,6 +58,25 @@ class Inbox extends Component {
         });
       });
     }
+    // create cards
+    const cards = inbox.cards;
+    cards.forEach(card => {
+      const cardContainer = document.createElement("div");
+      cardContainer.classList.add("card", "container");
+      cardContainer.dataset.key = card.id;
+      parent.appendChild(cardContainer);
+      new Card(cardContainer, { card });
+    });
+    // collapse cards if saved in localStorage
+    if (JSON.parse(localStorage.getItem(`${this.props.inbox.id}-collapsed`))) {
+      const children = this.parent.querySelectorAll(".card.container");
+      const actionsContainer = this.parent.querySelector(
+        "#inbox-action-indicator"
+      );
+      const headerActions = this.parent.querySelector(".inbox.actions");
+      const collapseButton = actionsContainer.querySelector(".inbox.collapse");
+      this.styleCollapse(children, headerActions, collapseButton);
+    }
   }
   clickAway = () => {
     const titleEdit = this.parent.querySelector(".as-h2");
@@ -77,6 +96,41 @@ class Inbox extends Component {
   };
   openForm = () => {
     this.setState({ showForm: !this.state.showForm });
+  };
+  toggleCollapse = () => {
+    const children = this.parent.querySelectorAll(".card.container");
+    const actionsContainer = this.parent.querySelector(
+      "#inbox-action-indicator"
+    );
+    const headerActions = this.parent.querySelector(".inbox.actions");
+    const collapseButton = actionsContainer.querySelector(".inbox.collapse");
+    if (JSON.parse(localStorage.getItem(`${this.props.inbox.id}-collapsed`))) {
+      this.styleExpand(children, headerActions, collapseButton);
+    } else {
+      this.styleCollapse(children, headerActions, collapseButton);
+    }
+  };
+  styleExpand = (children, headerActions, collapseButton) => {
+    children.forEach(child => {
+      child.style.display = "flex";
+    });
+    localStorage.setItem(`${this.props.inbox.id}-collapsed`, false);
+    headerActions.removeChild(this.parent.querySelector(".inbox.indicator"));
+    collapseButton.innerHTML = Expand();
+  };
+  styleCollapse = (children, headerActions, collapseButton) => {
+    children.forEach(child => {
+      child.style.display = "none";
+    });
+    localStorage.setItem(`${this.props.inbox.id}-collapsed`, true);
+    const inboxIndicator = document.createElement("h4");
+    inboxIndicator.classList.add("inbox", "indicator");
+    inboxIndicator.innerText = this.props.inbox.cards.length;
+    headerActions.insertBefore(
+      inboxIndicator,
+      headerActions.querySelector(".inbox.title")
+    );
+    collapseButton.innerHTML = Collapse();
   };
   updateField(updatedData) {
     const { inbox } = this.props;
@@ -111,7 +165,7 @@ class Inbox extends Component {
       });
     }
     if (!this.state.showForm && newCardForm) {
-      addButton.innerText = "➕";
+      addButton.innerHTML = Add();
       this.parent.removeChild(newCardForm);
     }
     this.sweepAndUpdate();
