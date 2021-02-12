b90bbfc21ad29e918485f17d03b0858a3fd2057d
taylor <jamestthompson3@gmail.com>
2021-02-12 16:54:44 +0200

    Removes CLI flags in favor of settings file.

--- env.sh
+++ env.sh
@@ -2,9 +2,11 @@
 if [[ $1 == 'test' ]]; then
   export EROSION_DIR=/tmp/erosion
   export EROSION_ENV=test
+  export EROSION_DBG=true
 fi
 
 if [[ $1 == 'dev' ]]; then
   export EROSION_ENV=dev
+  export EROSION_DBG=true
 fi
 
